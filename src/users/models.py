a7166cbfe4cb2817e88c64dc3ceb73f9e6dc0aea
johney.feng <johney.feng@cloud-interactive.com>
2021-11-10 15:26:17 +0800

    add User

--- src/users/models.py
+++ src/users/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class User(models.Model):
+    class Meta:
+        db_table = 'user'
+
+    line_user_id = models.TextField(default='0')
+    display_name = models.TextField()
+    picture_url = models.TextField()
+    status_message = models.TextField()
+    created_time = models.DateTimeField(auto_now_add=True)
+    last_login_time = models.DateTimeField()
--- src/users/views.py
+++ src/users/views.py
@@ -8,6 +8,11 @@ from linebot import LineBotApi, WebhookParser
 from linebot.exceptions import InvalidSignatureError, LineBotApiError
 from linebot.models import MessageEvent, TextSendMessage
 from utils.coupons import get_coupons_result
+
+from linebot import LineBotApi
+from linebot.exceptions import LineBotApiError
+from users.models import User
+from datetime import datetime
 line_bot_api = LineBotApi(settings.LINE_CHANNEL_ACCESS_TOKEN)
 parser = WebhookParser(settings.LINE_CHANNEL_SECRET)
 
@@ -16,9 +21,9 @@ parser = WebhookParser(settings.LINE_CHANNEL_SECRET)
 @csrf_exempt
 def callback(request):
     if request.method == 'POST':
+        now = datetime.now()
         signature = request.META['HTTP_X_LINE_SIGNATURE']
         body = request.body.decode('utf-8')
-
         try:
             events = parser.parse(body, signature)  # 傳入的事件
         except InvalidSignatureError:
@@ -27,6 +32,25 @@ def callback(request):
             return HttpResponseBadRequest()
 
         for event in events:
+            profile = line_bot_api.get_profile(event.source.user_id)
+            
+            
+            
+            user_qs = User.objects.filter(line_user_id=event.source.user_id)
+            if user_qs.exists():
+                user_obj = user_qs.first()
+                user_obj.last_login_time = now()
+                user_obj.save()
+            User.objects.create(
+                {
+                    "line_user_id" : event.source.user_id,
+                    "display_name" : profile.display_name,
+                    "picture_url" : profile.picture_url,
+                    "status_message" : profile.status_message,
+                    "last_login_time" : now,
+                }
+            )
+                
             if isinstance(event, MessageEvent):  # 如果有訊息事件
                 input_text = event.message.text
                 try:
